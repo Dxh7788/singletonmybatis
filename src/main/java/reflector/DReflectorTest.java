1e5f1f04660552e6d0773b068b12c34886378f5d
Dxh7788 <anan1215>
2018-03-21 13:30:50 +0800

    初读Mybatis源码,反射器

--- src/main/java/reflector/DReflectorTest.java
+++ src/main/java/reflector/DReflectorTest.java
@@ -1,7 +1,11 @@
 package reflector;
 
 import org.and.data.model.User;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.Reflector;
+import org.apache.ibatis.session.Configuration;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 /**
@@ -18,6 +22,21 @@ public class DReflectorTest {
         }
         for (Map.Entry<String,String> sm:reflector.getMethods.entrySet()){
             System.out.println(sm.getKey()+"的get方法是:"+sm.getValue());
+        }
+
+        //使用Mybatis Configuration默认属性创建类
+        Configuration configuration = new Configuration();
+        User user = new User();
+        MetaObject mo = configuration.newMetaObject(user);
+        Reflector rf=mo.getReflectorFactory().findForClass(User.class);
+        try {
+            //首先创建一个对象
+            rf.getSetInvoker("name").invoke(user,new Object[]{new String("123")});
+            System.out.println(rf.getGetInvoker("name").invoke(user,null));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
     }
 }
