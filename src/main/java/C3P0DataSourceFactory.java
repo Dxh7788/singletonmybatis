561e9068a453a8da83ff9d81b633059cb982a035
Dxh7788 <anan1215>
2018-03-21 11:12:38 +0800

    加入c3p0配置

---
+++ src/main/java/C3P0DataSourceFactory.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * Copyright (C) 2017-2018 https://www.htouhui.com - A project by singltonmybatis
+ *
+ * @author xh.d
+ * @since 2018/3/21 11:06
+ */
+public class C3P0DataSourceFactory {
+}
