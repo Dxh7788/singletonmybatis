efe0e0745b42dff8138d3f436ddebbaa7e0974d3
Dxh7788 <anan1215>
2018-03-23 11:26:35 +0800

    初读Mybatis源码分析,注释型缓存

--- src/main/java/org/and/data/mapper/UserMapper.java
+++ src/main/java/org/and/data/mapper/UserMapper.java
@@ -1,6 +1,11 @@
 package org.and.data.mapper;
 
+import org.and.cache.RedisCache;
 import org.and.data.model.User;
+import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.cache.decorators.LruCache;
 
 import java.util.List;
 import java.util.Map;
@@ -11,8 +16,10 @@ import java.util.Map;
  * @author xh.d
  * @since 2018/3/14 11:14
  */
+@CacheNamespace(implementation = RedisCache.class)
 public interface UserMapper {
     User selectByPrimaryKey(Long id);
     List<User> selectByNameAndPwd(String name, String pwd);
+    @Options()
     List<User> selectByNameAndPwd1(Map<String, String> map);
 }
